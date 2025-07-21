6bbc7233dff5694b1458506264e277a1fd1c5621
khgeung <khgeung5357@gmail.com>
2025-07-21 17:52:56 +0900

    feat : mybatis resultmap

---
+++ spring16-MyBatis-ResultMap-movie-director/src/main/java/org/kosa/myproject/runner/DirectorStudyRunner.java
@@ -0,0 +1,41 @@
+package org.kosa.myproject.runner;
+
+import java.util.List;
+import java.util.Map;
+
+import org.kosa.myproject.mapper.DirectorMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+//@Component //test 마무리 되어서 실행 안되게 주석 처리
+//@Order(2)
+public class DirectorStudyRunner implements CommandLineRunner {
+	private final DirectorMapper directorMapper;
+
+	public DirectorStudyRunner(DirectorMapper directorMapper) {
+		super();
+		this.directorMapper = directorMapper;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		System.out.println("DirectorStudyRunner");
+		/**
+		 * TABLE Director 의 리스트 정보를 받환받아 출력 org.kosa.myproject.mapper.DirectorMapper
+		 * DirectorMapper interface 와 DirectorMapper xml 을 정의하여 아래 메소드가 정상적으로 수행하여 출력되도록
+		 * 구현
+		 */
+//		List<Director> list = directorMapper.findDirectorAllList();
+//		for (Director director : list) {
+//			System.out.println(director);
+//		}
+		
+		// 통계 sql 조회
+//		List<Map<String, Object>> list = directorMapper.findDirectorStatistics();
+//		for(Map<String, Object> map:list) {
+//			//ROUND(AVG(m.attendance),0) AS averageAttendance 와 AS 로 별칭을 준 이름이 map의 key로 반환된다.
+//			System.out.println(map.get("directorId")+" "+map.get("directorName")+" "+map.get("totalAttendance")+" "+map.get("averageAttendance"));
+//		}
+	}
+}
