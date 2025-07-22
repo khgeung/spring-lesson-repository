f5851d3a4f77d2a5f4b320922e8374605b3bfa6a
khgeung <khgeung5357@gmail.com>
2025-07-22 16:29:16 +0900

    feat : springboot thymeleaf mybatis 연동

---
+++ spring18-Thymeleaf-MyBatis/src/main/java/org/kosa/myproject/Spring18ThymeleafMyBatisApplication.java
@@ -0,0 +1,13 @@
+package org.kosa.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring18ThymeleafMyBatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring18ThymeleafMyBatisApplication.class, args);
+	}
+
+}
